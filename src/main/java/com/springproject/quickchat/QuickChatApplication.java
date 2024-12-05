43069a2dae584a92f2536501a5fdebd876b49e37
widad123 <khatiri.widad@gmail.com>
2024-12-05 14:17:01 +0100

    init projet spring

---
+++ src/main/java/com/springproject/quickchat/QuickChatApplication.java
@@ -0,0 +1,13 @@
+package com.springproject.quickchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuickChatApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(QuickChatApplication.class, args);
+    }
+
+}
